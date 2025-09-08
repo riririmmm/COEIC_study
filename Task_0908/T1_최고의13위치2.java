dd820cf0f1ee15d48d51772cdb0e6137e07f5ed8
riririmmm <mr8774@smail.kongju.ac.kr>
2025-09-09 00:40:21 +0900

    0908 과제1 완료

--- Task_0908/T1_최고의13위치2.java
+++ Task_0908/T1_최고의13위치2.java
@@ -26,12 +26,13 @@ public class T1_최고의13위치2 {
 
                 for (int k = 0; k < N; k++) {   // 두번째 격자
                     for (int h = 0; h < N-2; h++) {
+
                         if (i == k && Math.abs(h - j) <= 2) {   // 격자가 겹치는 경우
                             continue;
                         }
 
-                        int cntA = arr[i][j] + arr[i][j+1] + arr[i][j+2];
-                        int cntB = arr[k][h] + arr[k][h+1] + arr[k][h+2];
+                        int cntA = arr[i][j] + arr[i][j+1] + arr[i][j+2];   // 첫번째 격자 합
+                        int cntB = arr[k][h] + arr[k][h+1] + arr[k][h+2];   // 두번째 격자 합
                         int cnt = cntA + cntB;
                         max = Math.max(max, cnt);
                     }
