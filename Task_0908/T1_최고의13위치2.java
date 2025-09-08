841b76997cceae002195d74279f716298628abdf
riririmmm <mr8774@smail.kongju.ac.kr>
2025-09-09 00:31:30 +0900

    과제 1번 완료

--- Task_0908/T1_최고의13위치2.java
+++ Task_0908/T1_최고의13위치2.java
@@ -19,5 +19,28 @@ public class T1_최고의13위치2 {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
+
+        int max = -1;
+        for (int i = 0; i < N; i++) {     // 첫번째 격자
+            for (int j = 0; j < N-2; j++) {
+
+                for (int k = 0; k < N; k++) {   // 두번째 격자
+                    for (int h = 0; h < N-2; h++) {
+                        if (i == k && Math.abs(h - j) <= 2) {   // 격자가 겹치는 경우
+                            continue;
+                        }
+
+                        int cntA = arr[i][j] + arr[i][j+1] + arr[i][j+2];
+                        int cntB = arr[k][h] + arr[k][h+1] + arr[k][h+2];
+                        int cnt = cntA + cntB;
+                        max = Math.max(max, cnt);
+                    }
+                }
+
+            }
+        }
+
+        System.out.println(max);
+        br.close();
     }
 }
